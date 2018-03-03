81f894b6a05e13501ca4e3a6cebae0378d69a993
JonHolt <jonathanholt92@gmail.com>
2018-03-03 14:44:15 -0700

    some extra touches to the dication page

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app     = express();
 var path    = require("path");
+var index = 1;
+var numberedChunks = {}
+
+// Middleware
+app.use(bodyParser.json());
 
 // Views
 app.get('/', function(req, res) {
@@ -17,6 +23,27 @@ app.get('/content/style.css', function(req, res) {
 app.get('/scripts/dictate.js', function(req, res) {
     res.sendFile(__dirname + '/scripts/dictate.js');
 })
+
+// API
+app.post('/api/sendChunk', function(req, res) {
+    // TODO: require password to be sent with each chuck
+    if (!req.body) {
+        res.sendStatus(400);
+    }
+    if (req.body.chunk) {
+        numberedChunks[index++] = req.body.chunk;
+    }
+    if (req.body.newline) {
+        numberedChunks[index++] = { newline: true }
+    }
+    console.log(numberedChunks);
+    res.sendStatus(200);
+});
+app.delete('/api/clearAll', function(req, res) {
+    numberedChunks = {};
+    index = 1;
+    res.sendStatus(200);
+});
 
 app.listen(3000);
 console.log('Running at port 3000');
--- scripts/dictate.js
+++ scripts/dictate.js
@@ -1,10 +1,47 @@
 (function() {
     const LINE_LENGTH = 20;
     const dictationArea = document.getElementById('dictation');
+    
+    // util
+    function sendContents(chunk, newline) {
+        newline = newline || false;
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/api/sendChunk');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onload = function() {
+            // Report failed send attempts
+            if (xhr.status !== 200) {
+                console.log(xhr.response);
+            }
+        };
+        xhr.send(JSON.stringify({
+            newline,
+            chunk
+        }));
+    }
+
     dictationArea.addEventListener('input', function(event) {
         var contents = event.target.value;
         if (contents.length > LINE_LENGTH) {
+        event.target.value = '';
             // Send one line of text to server and remove sent text from text area.
+            sendContents(contents);
+        }
+    });
+    dictationArea.addEventListener('keypress', function(event) {
+        var key = (event.keyCode ? event.keyCode : event.which);
+        if (key === 13) {
+            var contents = event.target.value;
+            sendContents(contents, true);
+            event.target.value = '';
+            event.preventDefault();
+        }
+    });
+    document.getElementById('clear').addEventListener('click', function(event) {
+        if (window.confirm('This will delete all captions for the current event.')) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('DELETE', '/api/clearAll');
+            xhr.send();
         }
     });
 })();
